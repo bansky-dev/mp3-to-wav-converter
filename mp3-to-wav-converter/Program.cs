62c96aef502aaecb9f92f1fb22f3df45e36ec5a0
Kociarz <dbednorz@tuta.io>
2024-10-09 13:06:47 +0200

    first commit

---
+++ mp3-to-wav-converter/Program.cs
@@ -0,0 +1,26 @@
+﻿using NAudio; 
+
+namespace mp3_to_wav_converter;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.Write("Please specify the input mp3 audio (full path): ");
+            string inputMp3File = Console.ReadLine();
+
+            Console.Write("Please speciy the output wav audio (full path): ");
+            string outputWavFile = Console.ReadLine();
+        }
+        catch(Exception ex)
+        {
+            System.Console.WriteLine($"ERROR CAUGHT: {ex}");
+        }
+        
+
+
+
+    }
+}
