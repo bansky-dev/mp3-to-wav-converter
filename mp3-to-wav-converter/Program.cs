fc3ae06db3376889686b9c1c856bb8a768d7d70a
Kociarz <dbednorz@tuta.io>
2024-10-09 21:00:49 +0200

    Finished

--- mp3-to-wav-converter/Program.cs
+++ mp3-to-wav-converter/Program.cs
@@ -1,24 +1,44 @@
-﻿using NAudio; 
+﻿using NAudio.Wave; 
 
 namespace mp3_to_wav_converter;
 
 class Program
 {
+    static string inputMp3File;
+    static string outputWavFile;
+
     static void Main(string[] args)
     {
         try
         {
             Console.Write("Please specify the input mp3 audio (full path): ");
-            string inputMp3File = Console.ReadLine();
+            inputMp3File = Console.ReadLine();
 
             Console.Write("Please speciy the output wav audio (full path): ");
-            string outputWavFile = Console.ReadLine();
+            outputWavFile = Console.ReadLine();
         }
         catch(Exception ex)
         {
             System.Console.WriteLine($"ERROR CAUGHT: {ex}");
         }
+
+        try
+        {
+            using(var reader = new Mp3FileReader(inputMp3File))
+            {
+                WaveFileWriter.CreateWaveFile(outputWavFile, reader);
+                System.Console.WriteLine("Succes!");
+            }
+
+        }
+        catch(Exception ex)
+        {
+            System.Console.WriteLine(ex);
+        }
         
+
+        //TODO: Improve error handling, and experience with using the app
+    
 
 
 
